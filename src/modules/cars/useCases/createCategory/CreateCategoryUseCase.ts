3291f248fa304d61d849cb98f773253e10bf3067
Iago Alexandre <iagoaap@outlook.com>
2022-11-17 20:40:11 -0300

    tratamento de erros

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
@@ -21,7 +22,7 @@ class CreateCategoryUseCase {
     );
 
     if (categoryAlreadyExists) {
-      throw new Error("Category Already Existis!");
+      throw new AppError("Category Already Existis!");
     }
 
     this.categoriesRepository.create({ name, description });
