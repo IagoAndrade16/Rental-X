f99c4bc158027e61a5b92c67ee459b1afc8d9b88
Iago Alexandre <iagoaap@outlook.com>
2022-11-17 19:23:54 -0300

    password encrypt

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -10,6 +10,11 @@ class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+
+    return user;
+  }
   async create({
     name,
     email,
--- src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -16,10 +17,17 @@ class CreateUserUseCase {
     password,
     drive_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       drive_license,
     });
   }
